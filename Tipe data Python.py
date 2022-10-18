19b803d93e98ab2929020ad71ebc6685521fa035
Marselino-Geradus <107174913+Marselino-Geradus@users.noreply.github.com>
2022-10-18 20:21:44 +0800

    Add files via upload

---
+++ Tipe data Python.py
@@ -0,0 +1,45 @@
+print("Tipe2 data dalam Python")
+print("=========================")
+print("=========================")
+print()
+#tipe data String
+a = "aku lagi belajar"
+print("Kalimat: {} merupakan tipe data String".format(a))
+print("-------------------------------------------")
+
+#tipe data Integer
+b = 159
+print("{} merupakan tipe data integer".format(b))
+print("-------------------------------------------")
+
+#tipe data Float
+c = 58.5
+print("{} merupakan tipe data float".format(c))
+print("-------------------------------------------")
+
+#tipe data Boolean
+d = True
+print("{} merupakan tipe data boolean".format(d))
+print("-------------------------------------------")
+
+#tipe data List
+e = ["a", "b", "c"]
+print("{} merupakan tipe data List".format(e))
+print("-------------------------------------------")
+
+#tipe data Tuple
+f = ("ngoding",'main',"belajar",2003)
+print("{} merupakan tipe data Tuple".format(f))
+print("-------------------------------------------")
+
+#tipe data Set
+g = {"mangga","apel", "jambu"}
+h = {
+    (1, 2, 3),
+    (4, 5, 6),
+    (7, 8, 9),
+         (0)
+}
+print("{} dan {} merupakan tipe data Set".format(g,h))
+print("-------------------------------------------")
+
